75d8a15027bc4b486111b30bf01e8b2c349d523d
Lars Dev <janlarsaustero@gmail.com>
2023-07-23 02:45:48 -0400

    mysql npm db connect
    created a separate user with standard authentication in mysql workbench

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const { faker } = require('@faker-js/faker');
 
+const mysql = require('mysql');
 
-for (let index = 0; index < 10; index++) {
-    console.log(faker.internet.email());
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'sars',
+    password: 'Password01',
+    database: 'join_us',
+    insecureAuth: true
+});
+
+connection.connect();
 
 
-}
+var q = 'select curdate() as date, curtime() as time, now() as now';
+connection.query(q, function(error,results,fields){
+    if(error) throw error;
+    console.log(results[0])
+});
+
+connection.end();
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(faker.internet.email());
+
+
+// }

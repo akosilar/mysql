1f9dc481beb49cbf8c4824bf5a2588735b58dec8
akosilar <55524614+akosilar@users.noreply.github.com>
2023-07-24 20:21:09 -0400

    example get routes

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -5,6 +5,16 @@ app.get("/", function (req, res) {
     res.send('you reached the home page')
 })
 
+app.get("/joke", function (req, res) {
+    var joke = 'what dog?'
+    res.send(joke)
+    console.log('requested joke route')
+})
+
+app.get("/random_num", (req, res) => {
+    var rand = Math.random()
+    res.send('7' + rand)
+})
 app.listen(8080, function () {
     console.log('server running on 8080!')
 })

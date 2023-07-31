ee2b263f63e2045ea80800c263c3e57657e41db4
akosilar <55524614+akosilar@users.noreply.github.com>
2023-07-31 18:58:36 -0400

    added css

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql')
+var mysql = require('mysql');
+var bodyParser = require('body-parser')
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -15,11 +20,25 @@ app.get("/", function (req, res) {
     var q = 'select count(*) as count from users;'
     connection.query(q, function (err, results) {
         if (err) throw err;
+        //respond with that count
         var count = results[0].count
-        res.send('we have ' + count + ' users in our db')
+        res.render('home', { count })
+        // res.send('we have ' + count + ' users in our db')
 
     })
-    //respond with that count
+})
+
+app.post('/register', (req, res) => {
+    const { email } = req.body
+    console.log(email)
+    //inserting data
+    var q = `insert into users (email) values ("${email}")`;
+
+    connection.query(q, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results.length, results)
+    });
+    res.redirect('/')
 })
 
 app.get("/joke", function (req, res) {
